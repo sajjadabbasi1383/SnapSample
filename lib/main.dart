4730205dca5da4a43d7d8ef01fdcd073e165671d
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2023-11-09 12:31:39 +0330

    Start Snap Project

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SafeArea(child: Scaffold(),)
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(child: Scaffold(body: Center(child: Text("Snap"),),),)
     );
   }
 }

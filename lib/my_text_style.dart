cceaec6baad2584cb06a6e72d0e357ae8b2f5a49
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2023-11-09 15:07:16 +0330

    Add Style Class & Dimens

---
+++ lib/my_text_style.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class MyTextStyle{
+  MyTextStyle._();
+
+  static TextStyle main=GoogleFonts.vazirmatn(fontSize: 20);
+}

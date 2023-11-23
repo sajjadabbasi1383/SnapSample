769843f6d17ff97bdb41978a3d33d297d973b6aa
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2023-11-23 14:55:12 +0330

    change buttomNavBar icon

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:snap_map/constant/my_text_style.dart';
 import 'package:snap_map/gen/assets.gen.dart';
 
@@ -12,53 +13,103 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return  SafeArea(
+    return SafeArea(
         child: DefaultTabController(
-          initialIndex: 3,
-          length: 4,
-          animationDuration: const Duration(milliseconds: 300),
-          child: Scaffold(
-      backgroundColor: Colors.grey,
-      appBar: AppBar(
-        bottom: const TabBar(
-          indicatorColor: Colors.black,
-          labelColor: Colors.black,
-          unselectedLabelColor: Colors.grey,
-          tabs: [
-            Tab(text: "سفارش ها",icon: Icon(Icons.shopping_cart_outlined,)),
-            Tab(text: "تخفیف ها",icon: Icon(Icons.discount_outlined,)),
-            Tab(text: "اسنپ کلاب",icon: Icon(Icons.diamond_outlined,)),
-            Tab(text: "خانه",icon: Icon(Icons.home,)),
-          ],
-        ),
+      initialIndex: 3,
+      length: 4,
+      animationDuration: const Duration(milliseconds: 300),
+      child: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          bottom: const TabBar(
+            indicatorColor: Colors.black,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey,
+            tabs: [
+              Tab(
+                  text: "سفارش ها",
+                  icon: Icon(
+                    Icons.shopping_cart_outlined,
+                  )),
+              Tab(
+                  text: "تخفیف ها",
+                  icon: Icon(
+                    Icons.discount_outlined,
+                  )),
+              Tab(
+                  text: "اسنپ کلاب",
+                  icon: Icon(
+                    Icons.diamond_outlined,
+                  )),
+              Tab(
+                  text: "خانه",
+                  icon: Icon(
+                    Icons.home,
+                  )),
+            ],
+          ),
           backgroundColor: Colors.white,
           elevation: 0,
           automaticallyImplyLeading: false,
           title: Row(
             children: [
-              const SizedBox(width: 5,),
-              const Icon(Icons.person_outline,size: 25,color: Colors.black,),
-              const SizedBox(width: 18,),
-              const Icon(Icons.credit_card_outlined,size: 24,color: Colors.black,),
-              const SizedBox(width: 18,),
-              const Icon(Icons.mail_outline,size: 24,color: Colors.black,),
-              const SizedBox(width: 18,),
+              const SizedBox(
+                width: 5,
+              ),
+              const Icon(
+                Icons.person_outline,
+                size: 25,
+                color: Colors.black,
+              ),
+              const SizedBox(
+                width: 18,
+              ),
+              const Icon(
+                Icons.credit_card_outlined,
+                size: 24,
+                color: Colors.black,
+              ),
+              const SizedBox(
+                width: 18,
+              ),
+              const Icon(
+                Icons.mail_outline,
+                size: 24,
+                color: Colors.black,
+              ),
+              const SizedBox(
+                width: 18,
+              ),
               const Expanded(child: SizedBox.shrink()),
-              Image.asset(Assets.images.logo.path,width: 74,),
-
+              Image.asset(
+                Assets.images.logo.path,
+                width: 74,
+              ),
             ],
           ),
+        ),
+        body: TabBarView(
+          children: [
+            const Tab(
+                text: "سفارش ها",
+                icon: Icon(
+                  Icons.shopping_cart_outlined,
+                )),
+            const Tab(
+                text: "تخفیف ها",
+                icon: Icon(
+                  Icons.discount_outlined,
+                )),
+            const Tab(
+                text: "اسنپ کلاب",
+                icon: Icon(
+                  Icons.diamond_outlined,
+                )),
+            homePage()
+          ],
+        ),
       ),
-      body:   TabBarView(
-        children: [
-          const Tab(text: "سفارش ها",icon: Icon(Icons.shopping_cart_outlined,)),
-          const Tab(text: "تخفیف ها",icon: Icon(Icons.discount_outlined,)),
-          const Tab(text: "اسنپ کلاب",icon: Icon(Icons.diamond_outlined,)),
-          homePage()
-        ],
-      ),
-    ),
-        ));
+    ));
   }
 }
 
@@ -85,20 +136,9 @@ class BottomNavBar extends StatelessWidget {
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 2, 207, 36),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.motorcycle_sharp,
-                          color: Colors.white,
-                          size: 30,
-                        )),
-                  ),
+                  SvgPicture.asset(Assets.icons.bike),
                   const SizedBox(
-                    height: 3,
+                    height: 2.7,
                   ),
                   Text(
                     "اسنپ باکس",
@@ -107,19 +147,14 @@ class BottomNavBar extends StatelessWidget {
                 ],
               ),
               Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.indigoAccent,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.shopping_bag_outlined,
-                          color: Colors.white,
-                          size: 30,
-                        )),
+                  const SizedBox(
+                    height: 4.7,
+                  ),
+                  Image.asset(
+                    Assets.icons.spdiscount.path,
+                    width: 52.2,
                   ),
                   const SizedBox(
                     height: 3,
@@ -127,55 +162,33 @@ class BottomNavBar extends StatelessWidget {
                   Text(
                     "سوپرمارکت",
                     style: MyTextStyle.textStyle2,
-                  )
+                  ),
                 ],
               ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.pinkAccent,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.local_dining,
-                          color: Colors.white,
-                          size: 30,
-                        )),
-                  ),
+                  SvgPicture.asset(Assets.icons.food),
                   const SizedBox(
                     height: 3,
                   ),
                   Text(
                     "غذا",
                     style: MyTextStyle.textStyle2,
-                  )
+                  ),
                 ],
               ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 2, 207, 36),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.local_taxi,
-                          color: Colors.white,
-                          size: 30,
-                        )),
-                  ),
+                  SvgPicture.asset(Assets.icons.taxi),
                   const SizedBox(
                     height: 3,
                   ),
                   Text(
                     "اسنپ",
                     style: MyTextStyle.textStyle2,
-                  )
+                  ),
                 ],
               ),
             ],
@@ -186,21 +199,26 @@ class BottomNavBar extends StatelessWidget {
   }
 }
 
-Widget homePage(){
-  return  Stack(
+Widget homePage() {
+  return Stack(
     children: [
-      Positioned(
-        child: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child:  Container(
-            //width: double.infinity,
-            height: 60,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                image:  DecorationImage(
-                    image: AssetImage(Assets.images.logo.path), fit: BoxFit.fill)),
+      SingleChildScrollView(
+        //TODO:physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              //width: double.infinity,
+              height: 72,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                      image: AssetImage(Assets.images.bannerHaraj.path),
+                      fit: BoxFit.fill)),
+            ),
           ),
-        ),
+        ]),
       ),
       const BottomNavBar(),
     ],
